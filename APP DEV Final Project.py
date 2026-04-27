41574ce7a81b5094afcab2ca6f884e0a7dc6e377
Jshaw98 <jshaw98@students.kennesaw.edu>
2026-04-26 20:05:41 -0400

    Add files via upload

---
+++ APP DEV Final Project.py
@@ -0,0 +1,10 @@
+print("Welcome to Study Tracker")
+
+study_history = []
+
+while True: 
+    print("\n1. Enter Study Session Stats")
+    print("2. View Study History")
+    print("3. Exit")
+
+    choice = input("Enter your choice between 1 and 3")
